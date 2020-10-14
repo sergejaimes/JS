11f1295675d399dad2944a666f495c1afbd61a67
Serge Jaimes <serge.jaimes@gmail.com>
2020-10-14 10:21:07 -0500

    Problem 3

--- Practice.js
+++ Practice.js
@@ -1,4 +1,5 @@
 //Practice Problem 1 for Eloquent Javascript
+//Triangle Increment Program
 
 /*
 var triangle = "#";
@@ -10,6 +11,7 @@ for (let counter = 1; counter <= 7; counter += 1){
 */
 
 //Practice Problem 2 for Eloquent Javascript
+//Number Divisible Program
 
 /*
 for (let numberCount = 1; numberCount <= 100; numberCount++){
@@ -27,4 +29,31 @@ for (let numberCount = 1; numberCount <= 100; numberCount++){
 
     }
 }
+*/
+
+//Practice Problem 3 for Eleoquent Javascript
+//Chess Board Program
+
+/*
+var board = "";
+var boardSize = 8;
+
+   for (var lineCount = 0; lineCount < boardSize; lineCount++) {
+       for (var spaceCount = 0; spaceCount < boardSize; spaceCount++) {
+           if (lineCount % 2 == 0) {
+                var black = "#";
+                var white = " ";
+           } 
+            else {
+                black = " ";
+                white = "#";
+            }
+           if (spaceCount % 2 == 0) {
+               board += white;
+           } else {
+               board += black;
+           }
+       }
+       board += "\n";
+   console.log(board);
 */
