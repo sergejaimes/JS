e522f2508775c1b78d45bf1e523f5f0eff8cfa8c
Serge Jaimes <serge.jaimes@gmail.com>
2020-10-14 08:34:28 -0500

    Triangle

--- Practice.js
+++ Practice.js
@@ -1,7 +1,11 @@
 //Practice Problem 1 for Eloquent Javascript
+
+/*
 var triangle = "#";
 
 for (let counter = 1; counter <= 7; counter += 1){
     console.log(triangle);
     triangle += ("#");
 }
+*/
+
