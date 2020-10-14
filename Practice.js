f31e715ca0a411aa3b5b6236038f1d84aac84364
Serge Jaimes <serge.jaimes@gmail.com>
2020-10-14 09:04:51 -0500

    Problem 2

--- Practice.js
+++ Practice.js
@@ -10,3 +10,21 @@ for (let counter = 1; counter <= 7; counter += 1){
 */
 
 //Practice Problem 2 for Eloquent Javascript
+
+/*
+for (let numberCount = 1; numberCount <= 100; numberCount++){
+    if (numberCount % 3 == 0 && numberCount % 5 == 0){
+        console.log("FizzBuzz");
+    }
+    else if (numberCount % 3 == 0){
+        console.log("Fizz");
+    }
+    else if (numberCount % 5 == 0){
+        console.log("Buzz");
+    }
+    else {
+        console.log(numberCount);
+
+    }
+}
+*/
